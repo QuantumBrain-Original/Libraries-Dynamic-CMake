64d66d882e6a06a01384b473770caaf95d5d8cb4
QuantumBrain-Original <Quantum.Brain.Original@gmail.com>
2024-12-19 17:00:54 +0300

    Library fix

--- ByeUser_Dynamic_CMake.h
+++ ByeUser_Dynamic_CMake.h
@@ -2,13 +2,7 @@
 #include <iostream>
 #include <string>
 
-#ifdef MYLIB_DLL_EXPORT
-	#define MYLIB_DLL __declspec(dllexport)
-#else
-	#define MYLIB_DLL __declspec(dllimport)
-#endif
-
-class MYLIB_DLL Leaver
+class Leaver
 {
 	public:
 		void leave(std::string name);
--- Libraries-Dynamic-CMake.cpp
+++ Libraries-Dynamic-CMake.cpp
@@ -3,7 +3,7 @@
 #include <Windows.h>
 #include "ByeUser_Dynamic_CMake.h"
 
-int main() // Задача 2. Динамическая библиотека
+int main() // Задача 3. Динамическая библиотека CMake
 {
 	SetConsoleCP(CP_UTF8);
 	SetConsoleOutputCP(CP_UTF8);
